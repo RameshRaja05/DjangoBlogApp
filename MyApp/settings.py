45e5abde82bcc858570611f05613375a9e0c8115
RameshRaja05 <118657318+RameshRaja05@users.noreply.github.com>
2023-04-19 13:12:34 +0530

    add a deploy settings

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -83,14 +83,10 @@ WSGI_APPLICATION = 'MyApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600 )
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
